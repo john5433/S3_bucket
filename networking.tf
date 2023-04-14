d6dd4915e4d8a9b80f3fb763d61076f0761bec6c
James Bond <jamesbond@gmail.com>
2023-04-14 06:24:59 +0000

    subnet

--- networking.tf
+++ networking.tf
@@ -6,3 +6,12 @@ resource "aws_vpc" "main-vpc" {
     }
 }
 
+resource "aws_subnet" "main-subnet" {
+   vpc_id     = aws_vpc.main-vpc.id
+   cidr_block = "10.100.0.0/24"
+   map_public_ip_on_launch = true
+   tags = {
+       Name = "john.007-subnet1"
+   }
+}
+
