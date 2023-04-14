9f06c58ee39821d9b264e8339036406ce9537719
James Bond <jamesbond@gmail.com>
2023-04-14 06:29:48 +0000

    route

--- output.tf
+++ output.tf
@@ -5,3 +5,15 @@ output "my_subnet" {
    value = aws_subnet.main-subnet.id
 }
 
+output "my-igw" {
+     value = aws_internet_gateway.igw.id
+}
+
+output "my-route-table" {
+   value = aws_route_table.my-route-table.id
+}
+
+output "my-route-table-association" {
+   value = aws_route_table_association.my-route-table-association.id
+}
+
